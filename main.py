aebb7ff3ccbb224e2c6c357a7297633b7c493061
Meher Deepak <68017128+MeherDeepak-2005@users.noreply.github.com>
2022-12-07 01:33:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import flask
 from werkzeug.utils import secure_filename
 from waitress import serve
 import mysql.connector
+from Search import Search
 from flask_cors import CORS, cross_origin, logging
 
-brochureUploadFilePath = './brochures'
-imageUploadFilePath = './images'
+cdn = './static'
 ALLOWED_EXTENSIONS = {'pdf'}
 
 app = flask.Flask(__name__)
 cors = CORS(app)
 
-app.config['BROCHURE_FOLDER'] = brochureUploadFilePath
-app.config['IMAGE_FOLDER'] = imageUploadFilePath
+app.config['CDN'] = cdn
 
 
 def newDb():
@@ -28,15 +27,38 @@ if database:
 
 db = database.cursor()
 
-create_projects_table = "create table projectLists if not exists (id int not null AUTO_INCREMENT primary key, city char(40), name varchar(20) unique, image varchar(200) unique, brochure varchar(200) unique);"
+create_projects_table = """
+CREATE TABLE IF NOT EXISTS products (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    city CHAR(40), name VARCHAR(20) UNIQUE,
+    image VARCHAR(200) UNIQUE,
+    brochure VARCHAR(200) UNIQUE
+);
+"""
 db.execute(create_projects_table)
 
-create_employee_table = "create table employee if not exists (id int not null AUTO_INCREMENT primary key, name char(20), mail char(30) not null UNIQUE, phone int(10) NOT NULL UNIQUE, designation char(20), password varchar(200) NOT NULL);"
+create_employee_table = """
+CREATE TABLE IF NOT EXISTS staff (id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(20),
+    mail CHAR(30) NOT NULL UNIQUE,
+    phone int(10) NOT NULL UNIQUE,
+    designation CHAR(20),
+    password VARCHAR(200) NOT NULL
+);"""
 db.execute(create_employee_table)
 
-create_queries_table = "create table queries if not exists (id int not null AUTO_INCREMENT primary key, client_email char(20), client_name char(20), client_phone int(10) NOT NULL, description varchar(200) not null);"
+create_queries_table = """
+CREATE TABLE IF NOT EXISTS queries (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    client_email CHAR(20),
+    client_name CHAR(20),
+    client_phone int(10) NOT NULL,
+    description VARCHAR(200) NOT NULL
+);"""
 db.execute(create_queries_table)
 
+
+# db.execute(createTables, multi=True)
 db.close()
 
 
@@ -62,7 +84,7 @@ def add(queryType):
     print(data)
     addcursor = database.cursor(buffered=True)
     if queryType == 'project':
-        addcursor.execute("INSERT into projectLists (name, city, image, brochure) VALUES (%s, %s, %s, %s)",
+        addcursor.execute("INSERT INTO projectLists (name, city, image, brochure) VALUES (%s, %s, %s, %s)",
                    (data['name'], data['city'], data['image'], data['brochure']))
         database.commit()
 
@@ -70,7 +92,7 @@ def add(queryType):
         addcursor.close()
 
     if queryType == 'query':
-        addcursor.execute("INSERT into queries (client_email, description, client_name, client_phone) VALUES (%s, %s, %s, %s)", (data['client_email'], data['description'], data['client_name'], data['client_phone']))
+        addcursor.execute("INSERT INTO queries (client_email, description, client_name, client_phone) VALUES (%s, %s, %s, %s)", (data['client_email'], data['description'], data['client_name'], data['client_phone']))
         database.commit()
         print(addcursor.rowcount, 'record inserted')
         addcursor.close()
@@ -88,42 +110,56 @@ def fetch(queryType):
         data = fetchcursor.fetchall()
         fetchcursor.close()
         return flask.jsonify(data), 200
-    if queryType == '':
+    if queryType == 'queries':
+        fetchcursor.execute(
+            "SELECT * FROM queries")
+        data = fetchcursor.fetchall()
+        fetchcursor.close()
+        return flask.jsonify(data), 200
+
         return 200
 
-@app.route('/cdn/view/<fileType>/id/<name>', methods=['GET'])
+@app.route('/cdn/view/<name>', methods=['GET'])
 @cross_origin()
-def view(fileType,name):
-    if fileType == 'brochure':
-        return flask.send_from_directory(app.config['BROCHURE_FOLDER'], filename=name)
-    if fileType == 'image':
-        return flask.send_from_directory(app.config['IMAGE_FOLDER'], filename=name)
+def view(name):
+    return flask.send_from_directory(app.config['CDN'], filename=name)
 
 @app.route('/upload', methods=['POST'])
 @cross_origin()
 def upload_file():
-    print("Request got")
+    imageNames = []
     if flask.request.method == 'POST':
         if flask.request.files:
-            brochure = flask.request.files['brochure']
-            brochure_name = secure_filename(brochure.filename)
-            brochure.save(os.path.join(app.config['BROCHURE_FOLDER'], brochure_name))
-
-            project_image = flask.request.files['image']
-            projectImage_name = secure_filename(project_image.filename)
-            project_image.save(os.path.join(app.config['IMAGE_FOLDER'], projectImage_name))
-
-
+            print(flask.request.files)
+            for productImage in flask.request.files:
+                productImage = flask.request.files[productImage]
+                image_name = secure_filename(productImage.filename)
+                imageNames.append(image_name)
+                productImage.save(os.path.join(app.config['CDN'], image_name))
         else:
             flask.flash('No file attached to the request')
 
-    return flask.jsonify({
-        "brochure_downloadURL": "http://127.0.0.1:5000" + flask.url_for('view', fileType='brochure', name=brochure_name),
-        "image_downloadURL": "http://127.0.0.1:5000" + flask.url_for('view', fileType='image', name=projectImage_name)
-    }), 201
+    imageUrls = [flask.url_for('view', name=imageName) for imageName in imageNames]
+    print(imageUrls)
+        #     brochure = flask.request.files['brochure']
+        #     brochure_name = secure_filename(brochure.filename)
+        #     brochure.save(os.path.join(app.config['BROCHURE_FOLDER'], brochure_name))
+        #
+        #     project_image = flask.request.files['image']
+        #     projectImage_name = secure_filename(project_image.filename)
+        #     project_image.save(os.path.join(app.config['IMAGE_FOLDER'], projectImage_name))
+        #
+        #
+        # else:
+        #     flask.flash('No file attached to the request')
+
+    return flask.jsonify({"Message": "OK"})
 
 if __name__ == '__main__':
     app.secret_key = 'MxMx28xAxE'
     app.config['SESSION_TYPE'] = 'filesystem'
-    serve(app, host='127.0.0.1', port=5000)
+    # serve(app, host='127.0.0.1', port=5000)
 
+    logging.getLogger('flask_cors').level = logging.DEBUG
+    #
+    app.run(debug=True)

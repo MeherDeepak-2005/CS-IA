d2a53e6eb09511dad3423fc446ea3a30d2d4b0f8
Meher Deepak <68017128+MeherDeepak-2005@users.noreply.github.com>
2022-12-12 06:20:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
               <h1 style={{ fontFamily: "cursive", fontWeight: "200", fontSize: '30px' }}>{product[1]}</h1>
               <h3 style={{ fontWeight: "400" }}>{product[2]}</h3>
               <div style={{ 'overflow': 'hidden', 'width': '20rem', height: "15rem", display: 'flex', alignItems: "center", justifyContent: "center" }}>
-                <img style={{ 'position': 'absolute', zIndex: '0', width: "20rem", height: "12rem" }} src={`http://127.0.0.1:5000/${product[3]}`} alt='product photos' />
+                <img style={{ 'position': 'absolute', zIndex: '0', width: "20rem", height: "12rem" }} src={`http://127.0.0.1:5000${product[3]}`} alt='product photos' />
                 <a target='_blank' rel='noreferrer' href={`/products/${product[0]}`} style={{ 'zIndex': '1', margin: 'auto' }}>
                   <button style={{ background: "#FAF089", border: 'none', padding: '0.5em', borderRadius: '10px' }}>More Information</button>
                 </a>
